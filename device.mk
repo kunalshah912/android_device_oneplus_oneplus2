5f0a49136a76b1a39ebc00ad6ae601a2bce63a6a
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-09-03 11:13:56 +0200

    oneplus2: add HIDL audio hal
    Change-Id: I2864fb7731a52893e6499164e87313e30362c965

--- device.mk
+++ device.mk
@@ -69,17 +69,22 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 # Audio
 PRODUCT_PACKAGES += \
-    audiod \
     audio.a2dp.default \
     audio.primary.msm8994 \
     audio.r_submix.default \
     audio.usb.default \
-    audio_policy.msm8994 \
     libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    tinymix
+    libvolumelistener \
+    libqcomvoiceprocessingdescriptors
+
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
